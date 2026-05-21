139a16c5393d48b22fa8002aaadbfeadd55863ea
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-21 16:55:25 +0200

    creato file data per i posts e fatto stampare chiamando con la sua rotta bacheca

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-import express from 'express';
+import express, { response } from 'express';
+import { posts } from './posts.js';
 
 const app = express();
 const port = 1106;
 const URL = 'localhost';
 
 app.get('/', (request, response) => {
+    // response.json({}) questa è una scorciatoia di quella sotto
     response
         .type('json')
-        .send({
-            messaggio: 'questo è un server di mia proprietà'
-        })
+        .send([
+            {
+                messaggio: 'questa è la home del mio BLOG'
+            }
+        ])
+});
+
+app.get('/bacheca', (request, response) => {
+    response.json({ posts })
 })
 
 app.listen(port, (error) => {
