557a57b1fa5988eeb4ecbc3e06451a1215b5bf33
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-21 16:17:19 +0200

    aggiunto metodo GET che manda un messaggio formato json al caricamento del server

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const port = 1106;
 const URL = 'localhost';
 
+app.get('/', (request, response) => {
+    response
+        .type('json')
+        .send({
+            messaggio: 'questo è un server di mia proprietà'
+        })
+})
+
 app.listen(port, (error) => {
     if (error) {
         console.error(error);
