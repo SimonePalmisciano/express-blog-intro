f08709233dd8110e7349f854f86e6d528c373aaa
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-21 16:07:36 +0200

    aggiunto watch nello scripts e aggiunta la possibilità di leggere env

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 1106;
+const URL = 'localhost';
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('server in ascolto sulla porta ' + port);
+    }
+})
